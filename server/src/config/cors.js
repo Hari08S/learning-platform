2fdd37c508b68dc0230c4094eb0349f23f8d679e
Hari08S <harisuresh82005@gmail.com>
2026-05-23 11:08:22 +0530

    updated cors origins

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,20 +1,39 @@
 // server/src/config/cors.js
 
-const allowedOrigins = [
-    process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
-    'http://localhost:5174',
-];
+const allowedOrigins = process.env.FRONTEND_ORIGIN
+  ? process.env.FRONTEND_ORIGIN.split(",")
+  : [
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        // allow requests with no origin (mobile apps, curl, etc.)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) return callback(null, true);
-        return callback(new Error('Not allowed by CORS'));
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: function (origin, callback) {
+    // allow requests with no origin
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+
+  credentials: true,
+
+  methods: [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+  ],
+
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+  ],
 };
 
 module.exports = corsOptions;
